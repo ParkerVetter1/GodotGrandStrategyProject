3c92a64862d88ca86ec73579135a0dc6ea2d280a
Parker Vetter <vette107@d.umn.edu>
2024-09-30 10:24:24 -0500

    Region Colors assigned no problem

--- Scripts/Province_Area.gd
+++ Scripts/Province_Area.gd
@@ -6,16 +6,13 @@ extends Area2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_child_entered_tree(node: Node) -> void:
 	if node.is_class("Polygon2D"):
 		node.color = Color(1,1,1,0.5)
-
 
 func _on_mouse_entered() -> void:
 	for node in get_children():
@@ -27,8 +24,6 @@ func _on_mouse_exited() -> void:
 		if node.is_class("Polygon2D"):
 			node.color = Color(1,1,1,0.5)
 
-
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		print(str(province_name) + " Clicked")
-		
---
+++ Scripts/RegionsManager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var regionColors = [Color.RED, Color.AQUA, Color.BLUE, Color.DARK_ORANGE, Color.WHITE, Color.PURPLE, Color.LIGHT_PINK]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_child_entered_tree(node: Node) -> void:
+	var randomNumb = randi() % regionColors.size() + 1 # returns a number between 0 and 7
+	node.modulate = regionColors[randomNumb]
+	regionColors.erase(regionColors[randomNumb])
+	
